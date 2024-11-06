dd44126b9351d9e540f7a91c7ab98106286e9957
Enzo Oliveira <ooliveiraenzo5@gmail.com>
2024-11-06 19:46:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            #region Exercicio Triangulo
+            //instancia da classe triangulo criada
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            //declaracao das variaveis
+            double areay, areax;
+
+            Console.WriteLine("Insira as medidas do Triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Insira as medidas do Triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //p = (x.A + x.B + x.C) / 2.0;
+
+            //x.Area = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            //p = (y.A + y.B + y.C) / 2.0;
+
+            //y.Area = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            areax = x.Area();
+            areay = y.Area();
+
+
+            Console.WriteLine("A area do triangulo X é: " + areax.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("A area do triangulo Y é: " + areay.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areax > areay) {
+                Console.WriteLine("Maior area: X");
+            } else {
+                Console.WriteLine("Maior area: Y");
+            }
+
+
+            #endregion
+
+            #region Exercicio Pessoa
+            //Pessoa p1 = new Pessoa();
+            //Pessoa p2 = new Pessoa();
+
+            //Console.WriteLine("Dados da primeira pessoa");
+            //p1.Nome = Console.ReadLine();
+            //p1.Idade = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Dados da segunda pessoa");
+            //p2.Nome = Console.ReadLine();
+            //p2.Idade = int.Parse(Console.ReadLine());
+            //if (p1.Idade > p2.Idade) {
+            //    Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            //} else {
+            //    Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            //}
+            #endregion
+
+            #region Exercicio funcionario
+            //Funcionario f1 = new Funcionario();
+            //Funcionario f2 = new Funcionario();
+
+            //double media;
+
+            //Console.WriteLine("Dados do primeiro funcionario");
+            //Console.Write("Nome: ");
+            //f1.Nome = Console.ReadLine();
+            //Console.Write("Idade: ");
+            //f1.Idade = int.Parse(Console.ReadLine());
+            //Console.Write("Salário: ");
+            //f1.Salario = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Dados do primeiro funcionario");
+            //Console.Write("Nome: ");
+            //f2.Nome = Console.ReadLine();
+            //Console.Write("Idade: ");
+            //f2.Idade = int.Parse(Console.ReadLine());
+            //Console.Write("Salário: ");
+            //f2.Salario = double.Parse(Console.ReadLine());
+
+            //media = (f1.Salario + f2.Salario) / 2.0;
+
+            //Console.WriteLine("Salario medio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            #endregion
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class Triangulo
+    {
+        //de acordo com o padrao do C# na poo nome de atributo sempre começa com letra maiúscula
+        public double A;
+        public double B;
+        public double C;
+
+        //metodos da classe triangulo
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+
+        
+    }
+}
