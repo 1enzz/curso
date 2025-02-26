5b9ad1b45d52f11c2fac3010da6ca90c190eb95a
Enzo Oliveira <enzo.oliveira@sislogica.com.br>
2025-02-26 16:27:20 -0300

    Criação de metodos nas funcoes; mexendo com overrride e tostring; Exercicio dos produtos

---
+++ Produto.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+namespace curso {
+    class Produto {
+
+        public string Nome;
+        public float Preco;
+        public int Quantidade;
+       
+
+        public void AdicionarProdutos (int quantidade){
+            Quantidade = Quantidade + quantidade;
+            
+
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade = Quantidade - quantidade;
+            
+           
+        }
+
+        public double ValorTotalEmEstoque() {
+            var valorTotal = Preco * Quantidade;
+            return valorTotal;
+        }
+
+        public override string ToString() {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + "unidades, Total: $" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,43 +9,43 @@ namespace curso
         static void Main(string[] args)
         {
             #region Exercicio Triangulo
-            //instancia da classe triangulo criada
-            Triangulo x = new Triangulo();
-            Triangulo y = new Triangulo();
+            ////instancia da classe triangulo criada
+            //Triangulo x = new Triangulo();
+            //Triangulo y = new Triangulo();
 
-            //declaracao das variaveis
-            double areay, areax;
+            ////declaracao das variaveis
+            //double areay, areax;
 
-            Console.WriteLine("Insira as medidas do Triangulo X: ");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Insira as medidas do Triangulo X: ");
+            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Insira as medidas do Triangulo Y: ");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //Console.WriteLine("Insira as medidas do Triangulo Y: ");
+            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //p = (x.A + x.B + x.C) / 2.0;
+            ////p = (x.A + x.B + x.C) / 2.0;
 
-            //x.Area = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            ////x.Area = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            //p = (y.A + y.B + y.C) / 2.0;
+            ////p = (y.A + y.B + y.C) / 2.0;
 
-            //y.Area = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            ////y.Area = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
-            areax = x.Area();
-            areay = y.Area();
+            //areax = x.Area();
+            //areay = y.Area();
 
 
-            Console.WriteLine("A area do triangulo X é: " + areax.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("A area do triangulo Y é: " + areay.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("A area do triangulo X é: " + areax.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("A area do triangulo Y é: " + areay.ToString("F4", CultureInfo.InvariantCulture));
 
-            if (areax > areay) {
-                Console.WriteLine("Maior area: X");
-            } else {
-                Console.WriteLine("Maior area: Y");
-            }
+            //if (areax > areay) {
+            //    Console.WriteLine("Maior area: X");
+            //} else {
+            //    Console.WriteLine("Maior area: Y");
+            //}
 
 
             #endregion
@@ -95,6 +95,57 @@ namespace curso
             //Console.WriteLine("Salario medio = " + media.ToString("F2", CultureInfo.InvariantCulture));
 
             #endregion
+
+            #region Exercicio Produtos
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            p.Preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            #region meu jeito
+            //Console.WriteLine("Dados do produtudo: " + p.Nome + ", $" + p.Preco + ", " + p.Quantidade + " unidades, Total: $ " + p.ValorTotalEmEstoque());
+
+            //Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            //p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            //Console.WriteLine("Dados atualizados: " + p.Nome + ", $" + p.Preco + ", " + p.Quantidade + " unidades, Total: $ " + p.ValorTotalEmEstoque());
+
+            //Console.WriteLine("Digite o número de produtos a ser removido ao estoque: ");
+            //p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            //Console.WriteLine("Dados atualizados: " + p.Nome + ", $" + p.Preco + ", " + p.Quantidade + " unidades, Total: $ " + p.ValorTotalEmEstoque());
+            #endregion
+
+            #region jeito correto com toString
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite a quantidade de produtos a serem adcionadas: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite a quantidade de produtos a serem removidos: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados: " + p);
+            #endregion
+            #endregion
+
+
+            #region Exercicios fixacao
+
+
+            #endregion
+
         }
     }
 }
